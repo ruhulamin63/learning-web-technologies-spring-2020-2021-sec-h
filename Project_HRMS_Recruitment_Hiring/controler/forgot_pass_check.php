7a8dc067a16d0fadde1630a325a80377b1f06245
Ruhul Amin <41449686+ruhulamin63@users.noreply.github.com>
2021-03-14 02:59:26 +0600

    user forgot password & update this password

---
+++ Project_HRMS_Recruitment_Hiring/controler/forgot_pass_check.php
@@ -0,0 +1,125 @@
+
+<!-- ========================================================================================= -->
+
+
+<?php 
+	$title= "Forgot Pss";
+	include('../view/header.html');
+?>
+
+	<table border="1px" align="center" width="100%">
+		<tr>	
+			<td>
+				<table width="100%">
+					<tr>
+						<td width="150px" height="50px">
+							<img src="../asset/logo.png" alt="main_logo" width="100%" height="100%">
+						</td>
+						<td align="right" >Logged in as
+							<a href="#">
+								
+							</a> |
+							<a href="#">Logout</a> 
+						</td>
+					</tr>
+				</table>
+			</td>
+		</tr>
+	</table>
+
+	<table border="1px" align="center" width="100%">
+		<tr>
+			<td width="200px" height="425px">MENU
+				<hr>
+				<details>
+					<summary><a href="#">Dashboard</a></summary>
+						
+				</details>
+
+				<details>
+					<summary>Screening & Approval</summary>
+						<details>
+							<summary><a href="#">Leave Approval</a></summary>
+						</details>
+						<details>
+							<summary><a href="#">Travel Approval</a></summary>
+						</details>
+						<details>
+							<summary><a href="#">Response of Grievances</a></summary>
+						</details>
+						<details>
+							<summary><a href="#">Performance Overview</a></summary>
+						</details>
+						<details>
+							<summary><a href="#">Loan Approval</a></summary>
+						</details>
+				</details>
+
+				<details>
+					<summary>Recruitment</summary>
+						<details>
+							<summary><a href="#">View Job Titles</a></summary>
+						</details>
+						<details>
+							<summary><a href="#">Add Job Vacancy</a></summary>
+						</details>
+						<details>
+							<summary><a href="#">View Job Vacancy</a></summary>
+						</details>
+						<details>
+							<summary><a href="#">Online Application</a></summary>
+						</details>
+						<details>
+							<summary><a href="#">Fixing Interview Online</a></summary>
+						</details>
+				</details>
+
+				<details>
+					<summary>Setting</summary>
+						<details>
+							<summary><a href="#">View Profile</a></summary>
+						</details>
+						<details>
+							<summary><a href="#">Edit Profile</a></summary>
+						</details>
+						<details>
+							<summary><a href="#">Change Password</a></summary>
+						</details>
+				</details>
+
+				<details>
+					<summary><a href="#">About</a></summary>
+				</details>
+
+				<details>
+					<summary><a href="#">Logout</a></summary>
+				</details>
+			</td>
+
+			<td>
+				<table align="center">
+					<tr>
+						<td>
+							<form method="post" action="#">
+								<fieldset>
+									<legend>Check Email</legend>
+									<table>
+										<tr>
+											<td>Enter Email</td>
+											<td>
+												<input type="text" name="curr_email" value="">
+											</td>
+										</tr>
+									</table>
+									<hr>
+									<input type="submit" name="forgot_pass_submit_button" value="Submit">
+								</fieldset>
+							</form>
+						</td>
+					</tr>
+				</table>
+			</td>
+		</tr>
+<?php 
+	include('../view/footer.html'); 
+?>
