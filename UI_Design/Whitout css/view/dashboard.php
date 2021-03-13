e0280ccfb33d6adb0eb64669f8b1dbae043d0a2e
Ruhul Amin <41449686+ruhulamin63@users.noreply.github.com>
2021-03-13 22:40:18 +0600

    UI design without css

---
+++ UI_Design/Whitout css/view/dashboard.php
@@ -0,0 +1,100 @@
+
+<?php 
+	$title= "Dashboard";
+	include('header.html');
+?>
+
+	<table border="1px" align="center" width="100%">
+		<tr>	
+			<td>
+				<table width="100%">
+					<tr>
+						<td width="150px" height="50px">
+							<img src="../asset/logo.png" alt="main_logo" width="100%" height="100%">
+						</td>
+						<td align="right" >Logged in as
+							<a href="#"></a> |
+							<a href="#">Logout</a> 
+						</td>
+					</tr>
+				</table>
+			</td>
+		</tr>
+	</table>
+<!-- new table creating -->
+	<table  border="1px" align="cen" width="100%">
+		<tr>
+			<td width="200px" height="425px">MENU
+				<hr>
+				<details>
+					<summary><a href="#">Dashboard</a></summary>
+						
+				</details>
+
+				<details>
+					<summary>Screening & Approval</summary>
+						<details>
+							<summary><a href="#">Leave Approval</a></summary>
+						</details>
+						<details>
+							<summary><a href="#">Travel Approval</a></summary>
+						</details>
+						<details>
+							<summary><a href="#">Response of Grievances</a></summary>
+						</details>
+						<details>
+							<summary><a href="#">Performance Overview</a></summary>
+						</details>
+						<details>
+							<summary><a href="#">Loan Approval</a></summary>
+						</details>
+				</details>
+
+				<details>
+					<summary>Requirement</summary>
+						<details>
+							<summary><a href="#">View Job Titles</a></summary>
+						</details>
+						<details>
+							<summary><a href="#">Add Job Vacancy</a></summary>
+						</details>
+						<details>
+							<summary><a href="#">View Job Vacancy</a></summary>
+						</details>
+						<details>
+							<summary><a href="#">Online Application</a></summary>
+						</details>
+						<details>
+							<summary><a href="#">Fixing Interview Online</a></summary>
+						</details>
+				</details>
+
+				<details>
+					<summary>Setting</summary>
+						<details>
+							<summary><a href="#">View Profile</a></summary>
+						</details>
+						<details>
+							<summary><a href="#">Edit Profile</a></summary>
+						</details>
+						<details>
+							<summary><a href="#">Change Password</a></summary>
+						</details>
+				</details>
+
+				<details>
+					<summary><a href="#">About</a></summary>
+				</details>
+
+				<details>
+					<summary><a href="#">Logout</a></summary>
+				</details>
+			</td>
+			<td colspan="2" align="center">
+				<h1>Welcome
+				</h1>
+			</td>
+		</tr>
+<?php 
+	include('footer.html'); 
+?>
