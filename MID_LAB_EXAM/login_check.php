3e74be7ab2332f5bf143ef09c2518d69732892cb
Ruhul Amin <41449686+ruhulamin63@users.noreply.github.com>
2021-03-14 13:29:56 +0600

    final commit at 1:30 PM

--- MID_LAB_EXAM/login_check.php
+++ MID_LAB_EXAM/login_check.php
@@ -1,3 +1,43 @@
+
+<?php
+	session_start();
+
+	if(isset($_POST['signin_btn'])){
+
+		$id = $_POST['id'];
+		$password = $_POST['password'];
+
+		if($username == "" || $password == ""){
+			echo "*Null submission...";
+		}else{
+
+			$myfile = fopen('data.json', 'r');
+			$data = fread($myfile, filesize('data.json'));
+			fclose($myfile);
+
+			$final_data = json_decode($data);
+			
+//===============================================================================
+			/*$user=$_SESSION['current_user'];*/
+//================================================================================			
+
+			if($id == $user['id'] && $password == $user['password']){
+
+				$_SESSION['flag'] = true;
+				
+				if(isset($_POST['user'])){
+					header('location: ')
+				}
+			}else{
+				echo "*Invalid user...";
+				//print_r($user);
+			}
+		}
+	}
+?>
+
+<!-- //================================================== -->
+
 <!DOCTYPE html>
 <html>
 <head>
