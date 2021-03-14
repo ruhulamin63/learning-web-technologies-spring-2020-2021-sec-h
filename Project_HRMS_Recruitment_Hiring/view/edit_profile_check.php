3411d720567fb6935e8d7fb6be1c8acaf74b2d0f
Ruhul Amin <41449686+ruhulamin63@users.noreply.github.com>
2021-03-14 11:24:48 +0600

    some add file

---
+++ Project_HRMS_Recruitment_Hiring/view/edit_profile_check.php
@@ -0,0 +1,115 @@
+<?php
+	session_start();
+
+	if(!isset($_SESSION['flag'])){
+		header('location: login_check.php');
+
+	}else{
+		$user=$_SESSION['current_user'];
+
+		$name=$user['name'];
+		$email=$user['email'];
+		$gender=$user['gender'];
+		$date=$user['date'];
+
+		if(isset($_POST['edit_profile_submit_button'])){
+			$name=$_POST['name'];
+			$name=$_POST['email'];
+			$name=$_POST['gender'];
+			$name=$_POST['date'];
+
+			$user['name']=$name;
+			$user['email']=$email;
+			$user['gender']=$gender;
+			$user['date']=$date;
+		}
+	}
+
+?>
+
+<!-- ================================================================================== -->
+
+
+<?php 
+	$title= "Edit Profile";
+	include('header.php');
+?>
+
+	<table border="1px" align="center" width="100%">
+		<tr>	
+			<td>
+				<table width="100%">
+					<tr>
+						<td width="200px" height="60px"><img src="../asset/logo.png" width="100%" height="100%"></td>
+						<td align="right" >
+							Logged in as
+							<a href="dashboard.php"> 
+								<?php
+									$user=$_SESSION['current_user'];
+									$name=$user['name'];
+									echo $name;
+								?>
+							</a> |
+							<a href="public_Home.html"> Logout </a> 
+						</td>
+					</tr>
+				</table>
+			</td>
+		</tr>
+	</table>
+
+	<table border="1px" align="center" width="100%">
+		<tr height="420px">
+			<td width="200px">Account<hr>
+				<ul>
+					<li><a href="dashboard.php">Dashboard</a></li>
+					<li><a href="view_profile_check.php">View Profile</a></li>
+					<li><a href="edit_profile_check.php">Edit Profile</a></li>
+					<!-- Add url in change profile pic -->
+					<li><a href="profile_pic.php">Change Profile Picture</a></li>
+					<li><a href="../controler/change_pass_check.php">Change Password</a></li>
+					<li><a href="public_Home.html">Logout</a></li>
+				</ul>
+			</td>
+			<td align="center">
+				<form method="post" action="edit_profile_check.php">
+					<fieldset>
+						<legend>EDIT PROFILE</legend>
+						<table>
+							<tr>
+								<td>Name</td>
+								<td>:<input type="name" name="name" value="<?php echo $name;?>"></td> 
+							</tr>
+							<tr>
+								<td>Email</td>
+								<td>:<input type="email" name="email" value="<?php echo $email;?>"></td> 
+							</tr>
+							<tr>
+								<td>Gender</td>
+								<td>:
+									<input type="radio" name="gender" <?php if (isset($gender) && $gender=="Male") echo "checked";?> value="Male"> Male
+									<input type="radio" name="gender" <?php if (isset($gender) && $gender=="female") echo "checked";?> value="Female"> Female
+									<input type="radio" name="gender" <?php if (isset($gender) && $gender=="Other") echo "checked";?> value="Other"> Other 
+								</td> 
+							</tr>
+							<tr>
+								<td>Date of Birth</td>
+								<td>:<input type="date" name="date" value="<?php echo $date;?>"></td> 
+							</tr>
+							<tr>
+								<!-- add line -->
+								<td><input type="submit" name="edit_profile_submit_button" value="Submit"></td>
+							</tr>
+						</table>
+					</fieldset>
+				</form>
+			</td>
+		</tr>
+		<tr height="50px">
+			<td colspan="2" align="center">
+				copyright@2021
+			</td> 
+		</tr>
+	</table>
+</body>
+</html>
