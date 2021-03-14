6f8d5fa0ae5b78d5863a788f85b1ad21e023e150
Ruhul Amin <41449686+ruhulamin63@users.noreply.github.com>
2021-03-14 12:39:22 +0600

    1st commit at 12:30 PM

---
+++ MID_LAB_EXAM/regCheck.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Registration</title>
+</head>
+<body>
+	<form>
+		<fieldset>
+			<legend>RIGIRSTRATION</legend>
+			<table>
+				<tr>
+					<td>
+						<table align="center" >
+							<tr>
+								<td>
+									<fieldset>
+										<table>
+											<tr>
+												<td>
+													Username<br>
+												</td>
+												<td>
+													<input type="text" name="username" value="">
+												</td>
+											</tr>
+											<tr>
+												<td>
+													Password
+												</td>
+												<td>
+													<input type="password" name="password" value="">
+												</td>
+											</tr>
+											<tr>
+												<td>
+													Confirm Password
+												</td>
+												<td>
+													<input type="password" name="cpassword" value="">
+												</td>
+											</tr>
+											<tr>
+												<td>
+													Name<br>
+												</td>
+												<td>
+													<input type="text" name="name" value="">
+												</td>
+											</tr>
+					
+											
+											<tr>
+												<td>
+													User Type
+												</td>
+												<td>
+													<input type="radio" name="gender" value="Male">User
+													<input type="radio" name="gender" value="Female">Admin
+												</td>
+											</tr>
+											<tr align="center">
+												<td colspan="2">
+													<br>
+												</td>
+											</tr>
+											<tr align="center">
+												<td colspan="2">
+													<hr>
+													<input type="submit" name="signup_btn" value="Sign Up">
+												</td>
+											</tr>
+										</table>
+									</fieldset>
+								</td>
+							</tr>
+						</table>
+					</td>
+				</tr>
+			</table>
+		</fieldset>
+	</form>
+</body>
+</html>
