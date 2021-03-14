05ede36c3a78bd6b6d18453b0bfca9f4ed290bef
Ruhul Amin <41449686+ruhulamin63@users.noreply.github.com>
2021-03-14 11:26:34 +0600

    adding profile picture file in php

---
+++ Project_HRMS_Recruitment_Hiring/view/profile_pic.php
@@ -0,0 +1,141 @@
+<?php
+	session_start();
+
+	if(!isset($_SESSION['flag'])){
+		header('location: login_check.php');
+	}else{
+
+		$user=$_SESSION['current_user'];
+		$name=$user['name'];
+		$email=$user['email'];
+		$gender=$user['gender'];
+		$date=$user['date'];
+//======================================================
+		
+	}
+?>
+
+<!-- ========================================================= -->
+
+<?php 
+	$title= "Picture Change";
+	include('header.php');
+?>
+
+	<table border="1px" align="center" width="100%">
+		<tr>	
+			<td>
+				<table width="100%">
+					<tr>
+						<td width="200px" height="60px"><img src="../asset/logo.png" width="100%" height="100%"></td>
+						<td align="right" >
+							Logged in as
+							<a href="dashboard.php"> 
+								<?php
+									$user=$_SESSION['current_user'];
+									$name=$user['name'];
+									echo $name;
+								?>
+							</a> |
+							<a href="public_Home.html"> Logout </a> 
+						</td>
+					</tr>
+				</table>
+			</td>
+		</tr>
+	</table>
+
+	<table border="1px" align="center" width="100%">
+		<tr>
+			<td width="200px" height="425px">MENU
+			<hr>
+				<details>
+					<summary><a href="#">Dashboard</a></summary>
+						
+				</details>
+
+				<details>
+					<summary>Screening & Approval</summary>
+						<details>
+							<summary><a href="#">Leave Approval</a></summary>
+						</details>
+						<details>
+							<summary><a href="#">Travel Approval</a></summary>
+						</details>
+						<details>
+							<summary><a href="#">Response of Grievances</a></summary>
+						</details>
+						<details>
+							<summary><a href="#">Performance Overview</a></summary>
+						</details>
+						<details>
+							<summary><a href="#">Loan Approval</a></summary>
+						</details>
+				</details>
+
+				<details>
+					<summary>Recruitment</summary>
+						<details>
+							<summary><a href="#">View Job Titles</a></summary>
+						</details>
+						<details>
+							<summary><a href="#">Add Job Vacancy</a></summary>
+						</details>
+						<details>
+							<summary><a href="#">View Job Vacancy</a></summary>
+						</details>
+						<details>
+							<summary><a href="#">Online Application</a></summary>
+						</details>
+						<details>
+							<summary><a href="#">Fixing Interview Online</a></summary>
+						</details>
+				</details>
+
+				<details>
+					<summary>Setting</summary>
+						<details>
+							<summary><a href="#">View Profile</a></summary>
+						</details>
+						<details>
+							<summary><a href="#">Edit Profile</a></summary>
+						</details>
+						<details>
+							<summary><a href="#">Change Password</a></summary>
+						</details>
+				</details>
+
+				<details>
+					<summary><a href="#">About</a></summary>
+				</details>
+
+				<details>
+					<summary><a href="#">Logout</a></summary>
+				</details>
+			</td>
+
+				<form method="post" action="view_profile_check.php" enctype="multipart/form-data">
+					<fieldset>
+						<legend>PROFILE</legend>
+						<table>
+							<tr>
+								<td>
+									<img src="../asset/user.png" width="200px" height="200px"><br>
+									<input type="file" name="choose_file" value="">
+								</td>
+							</tr>
+						</table>
+						<hr>
+						<input type="submit" name="submit_pic" value="Upload">
+					</fieldset>
+				</form>
+			</td>
+		</tr>
+		<tr height="50px">
+			<td colspan="2" align="center">
+				copyright@2021
+			</td> 
+		</tr>
+	</table>
+</body>
+</html>
