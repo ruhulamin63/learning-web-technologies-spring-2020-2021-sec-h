9dd9e24fc3e2cd0c2525545d02899b865278caa9
Ruhul Amin <41449686+ruhulamin63@users.noreply.github.com>
2021-03-23 13:09:48 +0600

    profile & view profile updated

--- FINAL_LAB_TASK_03_PHP/view/profile.php
+++ FINAL_LAB_TASK_03_PHP/view/profile.php
@@ -1,32 +1,44 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Profile</title>
+	<title>View Users</title>
 </head>
 <body>
 	<form>
 		<fieldset>
-			<table border="1px" align="center">
+			<table border="1px" align="center" cellpadding="10">
 				<tr>
-					<td colspan="2" align="center">
-						Profile
-					</td>
+					<td colspan="2" align="center">Profile</td>
 				</tr>
 				<tr>
-					<td>ID</td>
-					<td>16-10101-2</td>
+					<th>ID</th>
+					<th>NAME</th>
+					<th>EMAIL</th>
+					<th>USER TYPE</th>
 				</tr>
-				<tr>
-					<td>NAME</td>
-					<td>Bob</td>
-				</tr>
-				<tr>
-					<td>User Type</td>
-					<td>Admin</td>
-				</tr>
+					<?php
+						require_once('../model/db.php');
+
+						$get_id = $_GET['id'];
+
+						$id = getProductById($get_id);
+
+						if($row!=0){
+							while($value = mysqli_fetch_assoc($id)){
+								echo "
+									<tr>
+										<td>{$value['id']}</td>
+										<td>{$value['name']}</td>
+										<td>{$value['email']}</td>
+										<td>{$value['user']}</td>
+									</tr>
+								";
+							}
+						}
+					?>
 				<tr>
 					<td colspan="2" align="right">
-						<a href="home.admin_home.html">Go Home</a>
+						<a href="#">Go Home</a>
 					</td>
 				</tr>
 			</table>
--- FINAL_LAB_TASK_03_PHP/view/view_profile.php
+++ FINAL_LAB_TASK_03_PHP/view/view_profile.php
@@ -6,39 +6,45 @@
 <body>
 	<form>
 		<fieldset>
-			<table border="1px" align="center">
+			<table border="1px" align="center" cellpadding="10">
 				<tr>
-					<td colspan="3">Users</td>
+					<td colspan="4" align="center">Users</td>
 				</tr>
 				<tr>
-					<td>ID</td>
-					<td>NAME</td>
-					<td>USER TYPE</td>
+					<th>ID</th>
+					<th>NAME</th>
+					<th>EMAIL</th>
+					<th>USER TYPE</th>
 				</tr>
+					<?php
+						require_once('../model/db.php');
+
+						$conn = getConnection();
+						$sql = "select * from users";
+						$data = mysqli_query($conn,$sql);
+
+						$row = mysqli_num_rows($data);
+						//$result = mysqli_fetch_assoc($data);
+
+						//print_r($result);
+
+						if($row!=0){
+							while($result=mysqli_fetch_assoc($data)){
+								echo "
+									<tr>
+										<td>{$result['id']}</td>
+										<td>{$result['name']}</td>
+										<td>{$result['email']}</td>
+										<td>{$result['user']}</td>
+									</tr>
+								";
+							}
+						}
+					?>
 				<tr>
-					<td></td>
-					<td></td>
-					<td></td>
-				</tr>
-				<tr>
-					<td></td>
-					<td></td>
-					<td></td>
-				</tr>
-				<tr>
-					<td></td>
-					<td></td>
-					<td></td>
-				</tr>
-				<tr>
-					<td></td>
-					<td></td>
-					<td></td>
-				</tr>
-				<tr>
-					<td></td>
-					<td></td>
-					<td></td>
+					<td colspan="4" align="right">
+						<a href="#">Go Home</a>
+					</td>
 				</tr>
 			</table>
 		</fieldset>
